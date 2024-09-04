d28fddb32f9ef737a5da1d5f5876165294bd7851
Angel Olvera <angelolveraramirez999@gmail.com>
2024-09-03 23:54:46 -0600

    Stack heap

---
+++ Javascript/01-variables-data-types/05-stack-heap/script.js
@@ -0,0 +1,18 @@
+// Value is stored in the stack
+const name = 'John';
+const age = 30;
+
+// Reference is stored in the heap
+const person = {
+  name: 'Brad',
+  age: 40,
+};
+
+let newName = name;
+newName = 'Angel';
+
+let newPerson = person;
+newPerson.name = 'Olvera';
+
+console.log(name, newName);
+console.log(person, newPerson); 
