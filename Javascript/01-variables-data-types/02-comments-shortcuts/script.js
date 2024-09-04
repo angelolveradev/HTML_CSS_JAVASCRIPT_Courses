dcc53e5a796741fb126d8554619fc644e7336952
Angel Olvera <angelolveraramirez999@gmail.com>
2024-09-03 23:19:32 -0600

    Added Variable data types and arrays

---
+++ Javascript/01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,15 @@
+//this a single liena of code
+
+console.log(100);
+console.log("Hello World");
+const x = 100;
+
+console.log(x);
+
+console.warn('mames');
+
+console.table ({ name: 'Brad', email: 'brad@gmail.com'})
+
+const styles = 'padding: 10px; color: green; background-color:white';
+
+console.log('%cHello World', styles);
---
+++ Javascript/01-variables-data-types/03-variables/script.js
@@ -0,0 +1,52 @@
+// Ways to declare a variable
+// var, let, const
+let angelOlvera = 30;
+
+console.log(angelOlvera, kevinOlvera);
+
+var kevinOlvera = 'Kevin';
+
+console.log(kevinOlvera);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+
+if (true) {
+    score = score+=2
+}
+
+console.log(score);
+
+const array = [1,2,3,4];
+console.log(array);
+array.push = 5;
+console.log(array);
+
+const person = {
+    angel: 'olvera',
+};
+console.log(person.angel);
+
+person.angel = 'amish';
+person.email = 'angel@gmail.com'
+
+console.log(person);
+a
+
+
+
+
+
+// Naming Conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Can't start with a number
+
+// Multi-Word Formatting
+// firstName     camelCase
+// first_name    underscore
+// FirstName     PascalCase
+// firstname     owercase
