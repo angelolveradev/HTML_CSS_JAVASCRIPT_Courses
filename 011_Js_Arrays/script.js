9b5793603599e71fc791840f363573e84e26bba3
Angel Olvera <angelolveraramirez999@gmail.com>
2024-09-06 21:17:00 -0600

    -

--- 011_Js_Arrays/script.js
+++ 011_Js_Arrays/script.js
@@ -1,10 +1,15 @@
 // Making an array:
-const colors = ["red", "orange", "yellow"];
+const colors = ["red", "orange", "yellow","blue"];
 
+// Arrays are indexed like strings:
+colors[2]; // "red"
 
-
-
+// They have a length:
+colors.length; //3
 
 // Important array methods:
 //push(value) - adds value to the END of an array
 //pop() - removes and returns last value in array
+
+//unshift(val) - adds value to START of an array
+//shift() - removes and returns first element in an array
