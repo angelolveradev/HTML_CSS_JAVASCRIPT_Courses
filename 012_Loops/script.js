b83a3c705cebb71e876917b14d31cd1104af584e
Angel Olvera <angelolveraramirez999@gmail.com>
2024-09-10 00:03:51 -0600

    Loops

---
+++ 012_Loops/script.js
@@ -0,0 +1,22 @@
+/* console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10); */ 
+
+/*for (let i = 1; i<=10; i++){
+    console.log(i);
+}*/
+
+/*for (let u = 1; u<=6; u++){
+    console.log('Da ba dee da ba daa');
+}*/
+
+for (let v = 100; v>=0; v -= 10) {
+    console.log(v);
+}
